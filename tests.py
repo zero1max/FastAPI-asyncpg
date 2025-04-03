fa250f2c89480ca9ff222b3a067198700760e00e
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-04-03 11:52:46 +0500

    Update Test

--- tests.py
+++ tests.py
@@ -4,19 +4,19 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 def test_get():
-    response = client.get("/api/users")
+    response = client.get("/users")
     assert response.status_code == 200
 
 def test_post():
     data = {"full_name": "John Doe", "username": "john_doe", "email": "john.doe@example.com", "password": "secret123"}
-    response = client.post("/api/users", json=data)
+    response = client.post("/users", json=data)
     assert response.status_code == 200
 
 def test_put():
     data = {"full_name": "Jane Doe", "username": "jane_doe", "email": "jane.doe@example.com", "password": "secret123"}
-    response = client.put("/api/users/1", json=data)
+    response = client.put("/users?user_id=1", json=data)  # user_id query params sifatida berildi
     assert response.status_code == 200
 
 def test_delete():
-    response = client.delete("/api/users/1")
+    response = client.delete("/users?user_id=1")  # user_id query params sifatida berildi
     assert response.status_code == 200

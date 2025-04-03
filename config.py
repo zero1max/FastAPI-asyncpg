d28c2d01e7542ce18c6501a60f14579abc9492c9
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-04-03 10:09:38 +0500

    Update config

--- config.py
+++ config.py
@@ -9,4 +9,6 @@ DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 
-DATABASE_URL = f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL = f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+DATABASE_URL = f"sqlite:///{DB_NAME}.db"
